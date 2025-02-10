63d9661ee554a09724e5ecc51953e0ba4b9c69c2
ANTONIO\iscan <josemolinajaum@gmail.com>
2025-02-09 22:46:26 -0600

    agregar styles y boton agregar

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState("Frieren anime");
+
+  const onAddCategory = () => {
+    categories.push('OVerwatch');
+    console.log(categories);
+  }
+
   return (
     <>
         <h1>GifExpertApp</h1>
+
+        <button onClick={onAddCategory}>Agregar Categoria</button>
+
+        <ol>
+          { categories.map( category => {
+            return <li key={category}>{category}</li>
+          })}
+        </ol>
     </>
   )
 }
